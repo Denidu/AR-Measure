8982d1536bf098856422488b1e0556443144c521
Denidu Gamage <denidugamage21@gmail.com>
2025-07-03 10:07:43 +0530

    Initial Commit

---
+++ ARMeasure/ARMeasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARMeasureApp.swift
+//  ARMeasure
+//
+//  Created by Denidu Gamage on 2025-07-03.
+//
+
+import SwiftUI
+
+@main
+struct ARMeasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARMeasureTests/ARMeasureTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARMeasureTests.swift
+//  ARMeasureTests
+//
+//  Created by Denidu Gamage on 2025-07-03.
+//
+
+import Testing
+@testable import ARMeasure
+
+struct ARMeasureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
